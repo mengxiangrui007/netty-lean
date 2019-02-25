e654f4f83dd43ebb0dd64570f1fbdba764a6f025
mengxiangrui003 <mengxiangrui003@lianjia.com>
2019-02-25 18:03:22 +0800

    customer protocol

--- netty-lean-nio/src/main/java/com/tk/netty/nio/protocol/http/file/HttpFileServer.java
+++ netty-lean-nio/src/main/java/com/tk/netty/nio/protocol/http/file/HttpFileServer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.tk.netty.nio.protocol.http;
+package com.tk.netty.nio.protocol.http.file;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
